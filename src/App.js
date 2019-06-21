1166d3fd9dfef7c1c71bd42b014bca25ce5ccca0
LukeDaviesWeb <lukedaviesweb@gmail.com>
2019-06-21 11:26:41 +0800

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Calendar from "./Components/Calendar"
+
+import "./assets/style/App.css"
+
+function App() {
+	return (
+		<div className='App'>
+			<Calendar />
+		</div>
+	)
+}
+
+export default App
